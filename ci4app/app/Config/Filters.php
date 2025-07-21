0b40fa49d87c4658d6152b25c08c0e61c6006eb3
Yogi Pramana <161791924+IgoyAI@users.noreply.github.com>
2025-07-22 07:51:56 +0800

    Fix filter namespace

--- ci4app/app/Config/Filters.php
+++ ci4app/app/Config/Filters.php
@@ -12,6 +12,8 @@ use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\PageCache;
 use CodeIgniter\Filters\PerformanceMetrics;
 use CodeIgniter\Filters\SecureHeaders;
+use App\Filters\AuthFilter;
+use App\Filters\HrFilter;
 
 class Filters extends BaseFilters
 {
@@ -34,8 +36,8 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
-        'auth'          => App\Filters\AuthFilter::class,
-        'hr'            => App\Filters\HrFilter::class,
+        'auth'          => AuthFilter::class,
+        'hr'            => HrFilter::class,
     ];
 
     /**
